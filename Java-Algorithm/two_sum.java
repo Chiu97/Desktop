47e6bf13fa6480bc1c60585b9b669cc7b8f6d87d
Chiu97 <33931728+Chiu97@users.noreply.github.com>
2019-04-05 15:39:22 +0800

    \

---
+++ Java-Algorithm/two_sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      int[] res=new int[2];
+      int len=nums.length();
+      int i=len;
+      int num1,num2;
+      for(;i>0;i--){
+        if(nums[i]<target)
+          break;
+      }
+      int mid=i/2;
+      int j=0;
+      int x,y;
+      for(;j<mid;j++){
+        x=nums[i];
+      }
+      return res;
+    }
+}
---
+++ Python/001.py
@@ -0,0 +1,15 @@
+print("hello,world!")
+if 5>2:
+    print("Five is greater than two!")
+else:
+    print("Two is greater than five")
+#my first try for python
+carname="Volvo"
+print("car's name is "+carname)
+string1="i LOvE cat"
+print(string1[2:5].upper())
+print(string1.replace("i","U"))
+print(string1.split(" "))
+print("Please enter a number:")
+number = input()
+print("your number is " + number)
